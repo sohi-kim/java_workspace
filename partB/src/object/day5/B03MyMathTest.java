a0d2f1611fe084ef97c51c5f75ba5d589e418937
sohkim <koreait.sec2020@gmail.com>
2024-12-31 13:57:16 +0900

    day5 인스턴스 메소드

--- partB/src/object/day5/B03MyMathTest.java
+++ partB/src/object/day5/B03MyMathTest.java
@@ -9,13 +9,17 @@ public class B03MyMathTest {
       System.out.println("myMath.add() : " + myMath.add());
       System.out.println("myMath.substract() : " + myMath.substract());
 
+      System.out.println("=== print 메소드: type 값에 따라 +,-,* 중에서 연산 선택하고 출력 === ");
       myMath.print(1);    // 234 + 456 = 690
-      myMath.print(2);    // 234 + 456 = -222
-      myMath.print(3);    // 234 * 456 = .....
+      myMath.print(2);    // 234 - 456 = -222
+      myMath.print(3);    // 234 * 456 = 106704
 
-      MyMath temp = new MyMath();
-      System.out.println("temp.add() : " + temp.add());
-      System.out.println("temp.substract() : " + temp.substract());
+   
+
+      System.out.println("=== 초기값 테스트 ===");
+      MyMath temp = new MyMath();   // first, second 필드값이 처음부터 0 으로 저장.
+      System.out.println("temp.add() : " + temp.add());   // 0
+      System.out.println("temp.substract() : " + temp.substract()); //0
   }
 
 }
