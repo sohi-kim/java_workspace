a4ea8004e50fe915567cd95d0d5129baa6d8c579
sohkim <koreait.sec2020@gmail.com>
2025-01-02 15:14:29 +0900

    day6 열거형 enum

--- partB/src/object/day6/AnimalFarmEnum.java
+++ partB/src/object/day6/AnimalFarmEnum.java
@@ -1,25 +1,29 @@
 package object.day6;
 
-public class AnimalFarm {
+// Animal 의 인스턴스 필드 type 을 열거형 적용.
+public class AnimalFarmEnum {
     public static void main(String[] args) {
       
-      Animal maru = new Animal("DOG", "마루",12, "브라운");
-      Animal pepper = new Animal("CAT", "후추",4, "블랙");
-      Animal donald = new Animal("DUCK", "도날드",6, "그레이");
+      Animal2 maru = new Animal2(AnimalEnum.DOG, "마루",12, "브라운");
+      Animal2 pepper = new Animal2(AnimalEnum.CAT, "후추",4, "블랙");
+      Animal2 donald = new Animal2(AnimalEnum.DUCK, "도날드",6, "그레이");
+      // 열거형에 정의된 상수로만 값의 제한을 확실히 함.
+      // Animal donald2 = new Animal(AnimalEnum.RABBIT, "도날드",6, "그레이");
       System.out.println(maru.toString());
       System.out.println(pepper.toString());
       System.out.println(donald.toString());
     }
 }
 
-class Animal {
-    
-  private String type;
+class Animal2 {
+  
+  //String 형식을 열거형 AnimalEnum 으로 변경함. -> 생성자, getter,setter, sound 메소드 수정
+  private AnimalEnum type;
   private String name;
   private int age;
   private String color;
   
-  public Animal(String type, String name,int age, String color) {
+  public Animal2(AnimalEnum type, String name,int age, String color) {
     this.type = type;
     this.name = name;
     this.age = age;
@@ -29,17 +33,17 @@ class Animal {
 
   public String sound() {
     String result="";
-    switch (this.type) {
-      case "CAT":
+    switch (this.type) {   // type 필드는 AnimalEnum 형식. => 상수로 검사
+      case CAT:
         result  = "야옹야옹";
         break;
-      case "DOG":
+      case DOG:
         result="멍멍";
         break;
-      case "MOUSE":
+      case MOUSE:
         result="찍찍";
         break;
-      case "DUCK":
+      case DUCK:
         result="꽥꽥";
         break;
       default:
@@ -48,11 +52,11 @@ class Animal {
     return result;
   }
 
-  public String getType() {
+  public AnimalEnum getType() {
     return type;
   }
 
-  public void setType(String type) {
+  public void setType(AnimalEnum type) {
     this.type = type;
   }
 
