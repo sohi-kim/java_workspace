a8eba4bbdbae05bc821278087c84eb3687d7dacd
sohkim <koreait.sec2020@gmail.com>
2025-01-06 14:51:43 +0900

    day8 다형성 예시1

--- partB/src/object/day8/Animal.java
+++ partB/src/object/day8/Animal.java
@@ -10,9 +10,24 @@ public class Animal {
     System.out.println("동물 소리 들어보기//");
   }
 
+  // 부모클래스에서 정의한 toString 은 자식 클래스에 상속
+  @Override
+  public String toString() {
+    return "[aniName=" + aniName + ", age=" + age + ", color=" + color + ", hashCode()=" + hashCode() + "]";
+  }
+
+  
+
 }
 
 class Eagle extends Animal {
+
+  // 물려받은 인스턴스 필드의 초기값 저장
+  Eagle(){
+    this.aniName = "독수리";    //여기서는 this, super 모두 가능
+    this.age=15;
+    this.color="brown";
+  }
 
   // 1. 자식 클래스의 인스턴스 메소드
     public void fly(){
@@ -29,6 +44,12 @@ class Eagle extends Animal {
 }
 
 class Frog extends Animal {
+
+  Frog(){
+    this.aniName="청개구리";
+    this.age=1;
+    this.color="초록점박이";
+  }
   
   // 1. 자식 클래스의 인스턴스 메소드
   public void jump(){
@@ -44,6 +65,12 @@ class Frog extends Animal {
 }
 
 class Dog extends Animal {
+
+  Dog(){
+    this.aniName="푸들";
+    this.age=5;
+    this.color="밀키브라운";
+  }
 
   // 1. 자식 클래스의 인스턴스 메소드
   public void home(){
