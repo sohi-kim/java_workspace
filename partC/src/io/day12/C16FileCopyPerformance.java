fe401893a1cedfb0c6631d9656f3ff268e88f7a8
sohkim <koreait.sec2020@gmail.com>
2025-01-10 18:04:01 +0900

    day12 파일입출력

--- partC/src/io/day12/C16FileCopyPerformance.java
+++ partC/src/io/day12/C16FileCopyPerformance.java
@@ -20,12 +20,12 @@ public class C16FileCopyPerformance {
   public static void copyByByteArray(){   // 바이트배열을 이용하여 입출력단위를 개선
     FileInputStream fis = null;
     FileOutputStream fos = null;
-    byte[] buffer = new byte[1024];
+    byte[] buffer = new byte[1024];  //1kb
 
     try {
       int b;
       int count=0;
-      fis = new FileInputStream("C:\\Users\\Class01\\Downloads\\skyview.jpg");  
+      fis = new FileInputStream("C:\\Users\\Class01\\Downloads\\test.jpg");  
       fos = new FileOutputStream("C:\\Users\\Class01\\Downloads\\skyview_copy2.jpg");
       while((b=fis.read(buffer,0,buffer.length)) !=-1){   // 파일의 끝 EOF 는 -1
         // 실제 읽은 바이트 수는 b 에 저장.
