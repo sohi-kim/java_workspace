3ddb7b48f9751c92aa9d361eb496d53cd7dfc48a
sohkim <koreait.sec2020@gmail.com>
2025-01-13 11:12:24 +0900

    day13 문자기반입력

--- partC/src/io/day13/C17FileReaderTest.java
+++ partC/src/io/day13/C17FileReaderTest.java
@@ -1,30 +1,47 @@
 package io.day13;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class C17FileReaderTest {
 
   public static void main(String[] args) {
 
-    readByLine();
+    readUTF();
     
   } // main end
 
+  //3.  파일 인코딩 방식에 따라 읽어오기
   public static void readUTF(){
     String line = null;
     int count =0;
     InputStreamReader isr = null;
     Scanner sc = null;
+    String filename = "입출력.md";    // UTF-8 인코딩 파일
     try {
-
+      isr = new InputStreamReader(new FileInputStream(filename),StandardCharsets.UTF_8);
+      int ch;
+      while((ch=isr.read()) !=-1){
+        System.out.print((char)ch);    // 파일에 엔터 있을 때 줄바꿈
+        count++;
+      }
+      System.out.println("입력 문자 갯수 : " + count);
     }catch (Exception e) {
-      
+
+    }finally{
+      try {
+        isr.close();
+        
+      } catch (Exception e) {
+      }
     }
 
   }
 
+  // 2. 텍스트 파일 라인(줄) 단위로 읽어오기
   public static void readByLine(){
     // int ch;
     String line=null;
@@ -33,7 +50,7 @@ public class C17FileReaderTest {
     Scanner sc = null;
     StringBuilder sb = new StringBuilder();
     try {
-      fr = new FileReader("입출력.md");  //C:\\Class241129\\연습.txt
+      fr = new FileReader("C:\\Class241129\\연습.txt");  //C:\\Class241129\\연습.txt
       sc = new Scanner(fr);  //파일을 읽기 위해서 Reader 타입 클래스를 인자로 합니다.
       // hasXXX 은 iterator(반복자) 를 통해서 다음 내용이 있을 때 동작하는 방식
       // Iterator 인터페이스 구현 클래스 : hasNext() 메소드, nextXXX() 메소드가 있습니다.
@@ -55,6 +72,7 @@ public class C17FileReaderTest {
     }
   }
 
+  // 1. 텍스트 파일을 한 글자씩 읽어오기
   public static void readText(){
     int ch;
     int count=0;
